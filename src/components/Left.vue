748c67aad2dbd0743d394ff18da4128ca2a24f81
realeve@qq.com <realeve@qq.com>
2018-06-10 17:29:48 +0800

    init

---
+++ src/components/Left.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-chart class="margin-top-10" />
+</template>
+
+<script>
+import VChart from "./Chart/Bar.vue";
+
+export default {
+  components: {
+    VChart
+  }
+};
+</script>
+
+<style scoped lang="less">
+.margin-top-10 {
+  margin-top: 10px;
+}
+</style>
